99c782ec3602f43242a6c1e863d8bf6fbbf72d42
João Paulo Moraes <joaopaulomoraes@outlook.com>
2018-06-13 14:34:55 -0300

    feat: Theme creator

---
+++ src/utils/theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+import {
+  blue,
+  blueGrey
+} from '@material-ui/core/colors'
+
+const Theme = createMuiTheme({
+  palette: {
+    primary: blue,
+    secondary: blueGrey
+  }
+})
+
+export default Theme
