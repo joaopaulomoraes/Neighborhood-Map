46591fdba683be9d257aa34a9c9b3db895fca69d
João Paulo Moraes <joaopaulomoraes@outlook.com>
2018-06-13 21:28:00 -0300

    feat: Move to components and create dashboard

---
+++ src/components/App.js
@@ -0,0 +1,168 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import {
+  withStyles,
+  MuiThemeProvider
+} from '@material-ui/core/styles'
+import Drawer from '@material-ui/core/Drawer'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
+import Hidden from '@material-ui/core/Hidden'
+import Divider from '@material-ui/core/Divider'
+import MenuIcon from '@material-ui/icons/Menu'
+import Place from '@material-ui/icons/Place'
+import Theme from '../utils/theme'
+import { loadMapJS } from '../utils/helpers'
+
+const drawerWidth = 320
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    zIndex: 1,
+    overflow: 'hidden',
+    position: 'relative',
+    display: 'flex',
+    width: '100%'
+  },
+  appBar: {
+    position: 'absolute',
+    marginLeft: drawerWidth,
+    [theme.breakpoints.up('md')]: {
+      width: `calc(100% - ${drawerWidth}px)`
+    }
+  },
+  navIconHide: {
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    }
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+    [theme.breakpoints.up('md')]: {
+      position: 'relative'
+    }
+  },
+  content: {
+    flexGrow: 1,
+    height: '100vh',
+    backgroundColor: theme.palette.background.default
+  }
+})
+
+class App extends Component {
+  state = {
+    mobileOpen: false
+  }
+
+  componentDidMount() {
+    window.initMap = this.initMap
+    loadMapJS("https://maps.googleapis.com/maps/api/js?key=AIzaSyCacmIGcWsolGnXLp71cBM_My9axyprocM&callback=initMap")
+  }
+
+  initMap() {
+    const google = window.google
+    const map = new google.maps.Map(document.getElementById("map"), {
+      center: {
+        // Silicon Valley, CA, USA
+        lat: 37.387474,
+        lng: -122.057543
+      },
+      zoom: 12
+    })
+  }
+
+  handleDrawerToggle = () => {
+    this.setState({ mobileOpen: !this.state.mobileOpen })
+  }
+
+  render() {
+    const { classes, theme } = this.props
+
+    const drawer = (
+      <div>
+        <div className={classes.toolbar} />
+        <Divider />
+        <List>
+          <ListItem button>
+            <ListItemIcon>
+              <Place />
+            </ListItemIcon>
+            <ListItemText primary="Place" />
+          </ListItem>
+        </List>
+      </div>
+    )
+
+    return (
+      <div className={classes.root}>
+        <CssBaseline />
+        <MuiThemeProvider theme={Theme}>
+          <AppBar className={classes.appBar}>
+            <Toolbar>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                onClick={this.handleDrawerToggle}
+                className={classes.navIconHide}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="title" color="inherit" noWrap>
+                Neighborhood Map
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Hidden mdUp>
+            <Drawer
+              variant="temporary"
+              anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+              open={this.state.mobileOpen}
+              onClose={this.handleDrawerToggle}
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+              ModalProps={{
+                keepMounted: true // Better open performance on mobile.
+              }}
+            >
+              {drawer}
+            </Drawer>
+          </Hidden>
+          <Hidden smDown implementation="css">
+            <Drawer
+              variant="permanent"
+              open
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+            >
+              {drawer}
+            </Drawer>
+          </Hidden>
+          <main className={classes.content}>
+            <div id="map" className={classes.content} />
+          </main>
+        </MuiThemeProvider>
+      </div>
+    )
+  }
+}
+
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired
+}
+
+export default withStyles(
+  styles,
+  { withTheme: true }
+)(App)
