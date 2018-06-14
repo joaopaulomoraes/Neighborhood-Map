b9e0e427bf9d0df416e3b02a38e2a3ab9eb74f45
João Paulo Moraes <joaopaulomoraes@outlook.com>
2018-06-14 16:12:55 -0300

    feat: Added locations

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -6,3 +6,55 @@ export const loadMapJS = src => {
   script.onerror = () => document.write("Error to loading Google Maps API")
   ref.parentNode.insertBefore(script, ref)
 }
+
+export const locations = [
+  {
+    title: 'Lanches Vaz',
+    location: {
+      lat: -22.8871538,
+      lng: -48.4450821
+    }
+  },
+  {
+    title: 'The Hop Club',
+    location: {
+      lat: -22.885889,
+      lng: -48.443462
+    }
+  },
+  {
+    title: 'Brooks Hamburgueria',
+    location: {
+      lat: -22.885775,
+      lng: -48.442486
+    }
+  },
+  {
+    title: 'Salgados e Salgados',
+    location: {
+      lat: -22.8860484,
+      lng: -48.4427503
+    }
+  },
+  {
+    title: 'Alquimia Burguer Artesanal',
+    location: {
+      lat: -22.8865892,
+      lng: -48.4443522
+    }
+  },
+  {
+    title: 'VitStela - Sabores do Mundo',
+    location: {
+      lat: -22.8866214,
+      lng: -48.4441476
+    }
+  },
+  {
+    title: 'Pessin',
+    location: {
+      lat: -22.8837139,
+      lng: -48.4405039
+    }
+  }
+]
