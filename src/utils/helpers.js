f89c9043d433733c6bc046ab77329173d27fa615
João Paulo Moraes <joaopaulomoraes@outlook.com>
2018-06-15 01:51:23 -0300

    feat: Added personal style to map

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -58,3 +58,90 @@ export const locations = [
     }
   }
 ]
+
+export const mapStyles = [
+	{
+		featureType: 'administrative',
+		elementType: 'labels.text.fill',
+		stylers: [
+			{
+				color: '#444444'
+			}
+		]
+	},
+	{
+		featureType: 'landscape',
+		elementType: 'all',
+		stylers: [
+			{
+				color: '#f2f2f2'
+			}
+		]
+	},
+	{
+		featureType: 'poi',
+		elementType: 'all',
+		stylers: [
+			{
+				visibility: 'off'
+			}
+		]
+	},
+	{
+		featureType: 'road',
+		elementType: 'all',
+		stylers: [
+			{
+				visibility: 'simplified'
+			},
+			{
+				lightness: '19'
+			},
+			{
+				saturation: '-85'
+			},
+			{
+				gamma: '0.90'
+			}
+		]
+	},
+	{
+		featureType: 'road.highway',
+		elementType: 'all',
+		stylers: [
+			{
+				visibility: 'simplified'
+			}
+		]
+	},
+	{
+		featureType: 'road.arterial',
+		elementType: 'labels.icon',
+		stylers: [
+			{
+				visibility: 'off'
+			}
+		]
+	},
+	{
+		featureType: 'transit',
+		elementType: 'all',
+		stylers: [
+			{
+				visibility: 'simplified'
+			}
+		]
+	},
+	{
+		featureType: 'water',
+		elementType: 'all',
+		stylers: [
+			{
+				color: '#79d4fa'
+			},
+			{
+				visibility: 'on'
+			}
+		]
+	}
+]
